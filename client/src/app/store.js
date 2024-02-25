df7f5f59a4c86af233b0c064da6956172237af9c
Dale <Danntabachoyy@gmail.com>
2024-02-25 21:21:05 +0800

    init redux-toolkit, next: create postsSlice

---
+++ client/src/app/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { apiSlice } from './apiSlice';
+export const store = configureStore({
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }).concat(
+      apiSlice.middleware
+    ),
+});
