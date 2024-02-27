567971df2fef2c826577930955416d628fe47afc
Dale <Danntabachoyy@gmail.com>
2024-02-27 23:21:36 +0800

    refactor postExcerpt

--- client/src/features/posts/PostExcerpt.jsx
+++ client/src/features/posts/PostExcerpt.jsx
@@ -1,12 +1,47 @@
 import React from 'react';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import { FaRegComment, FaRegHeart } from 'react-icons/fa';
+import { FaHeart, FaRegComment, FaRegHeart } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { selectPostById } from './postSlice';
+import {
+  selectPostById,
+  useGetPostQuery,
+  useInteractToPostMutation,
+} from './postSlice';
 
 const PostExcerpt = ({ postId }) => {
-  const post = useSelector((state) => selectPostById(state, postId));
+  const { data: post, isSuccess } = useGetPostQuery(postId);
+  const [interactToPost] = useInteractToPostMutation();
+  const userId = 'dambo';
+  let heartReact;
+  if (isSuccess) {
+    const likeCounts = post.likes.length;
+    const userLikeThePost = post.likes.findIndex(
+      (likeUserId) => likeUserId === userId
+    );
+    if (userLikeThePost === -1) {
+      heartReact = (
+        <>
+          <div className="group-hover:bg-opacity-20 p-3 cursor-pointer rounded-full group-hover:bg-[#F91880]">
+            <FaRegHeart className="group-hover:text-[#F91880]" />
+          </div>
+          <p className="group-hover:text-[#F91880]">{likeCounts}</p>
+        </>
+      );
+    } else {
+      heartReact = (
+        <>
+          <div className="group-hover:bg-opacity-20 p-3 cursor-pointer rounded-full group-hover:bg-[#F91880]">
+            <FaHeart className="text-[#F91880]" />
+          </div>
+          <p className="text-[#F91880]">{likeCounts}</p>
+        </>
+      );
+    }
+  }
+  const handleClickLike = async () => {
+    await interactToPost({ postId, userId });
+  };
   return (
     <div className="p-4 flex cursor-pointer space-x-2 hover:bg-slate-100 border-t">
       <div>
@@ -21,8 +56,8 @@ const PostExcerpt = ({ postId }) => {
           <p className="text-sm text-slate-500">@MrDaleCabarle</p>
           <p className="text-sm text-slate-500">- 1h ago</p>
         </div>
-        {post.content ? (
-          <p>{post.content}</p>
+        {post?.content ? (
+          <p>{post?.content}</p>
         ) : (
           <p>
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio
@@ -30,10 +65,12 @@ const PostExcerpt = ({ postId }) => {
             numquam totam?
           </p>
         )}
-        <div className="flex space-x-2">
-          <div className="text-slate-500 flex space-x-1 items-center">
-            <FaRegHeart />
-            <p>22k</p>
+        <div className="flex space-x-4">
+          <div
+            className="text-slate-500 flex items-center group cursor-pointer"
+            onClick={handleClickLike}
+          >
+            {heartReact}
           </div>
           <div className="text-slate-500 flex space-x-1 items-center">
             <FaRegComment />
