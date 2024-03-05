08e00265ef13ffb7d1dff6d84085fd0c4670ee6c
Dale <Danntabachoyy@gmail.com>
2024-03-06 06:13:03 +0800

    update ui

--- client/src/comp/Layout.jsx
+++ client/src/comp/Layout.jsx
@@ -4,17 +4,33 @@ import Navbar from './Navbar';
 import SideBar from './SideBar';
 const Layout = () => {
   return (
-    <div className="w-4/5 min-h-screen mx-auto flex justify-between">
-      <div className="w-1/4 ">
-        <Navbar />
+    <div className="w-4/5 mx-auto h-screen flex">
+      <div className="w-1/4 h-full">
+        <div className="h-full flex flex-col">
+          <Navbar />
+        </div>
       </div>
-      <div className="flex-1 border-x">
+      <div className="flex-1 overflow-y-auto">
         <Outlet />
       </div>
-      <div className="w-1/4 p-4">
-        <SideBar />
+      <div className="w-1/4 h-full p-4">
+        <div className="h-full flex flex-col">
+          <SideBar />
+        </div>
       </div>
     </div>
+
+    // <div className="w-4/5 min-h-screen mx-auto flex ">
+    //   <div className="w-1/4 max-h-screen flex-shrink-0">
+    //     <Navbar />
+    //   </div>
+    //   <div className="flex-1 border-x overflow-y-auto">
+    //     <Outlet />
+    //   </div>
+    //   <div className="w-1/4 max-h-screen flex-shrink-0 p-4">
+    //     <SideBar />
+    //   </div>
+    // </div>
   );
 };
 
