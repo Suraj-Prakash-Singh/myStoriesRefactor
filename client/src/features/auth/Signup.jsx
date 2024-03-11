a7273e6617672d91eb4ea4c0bff4e02443b880e6
Dale <Danntabachoyy@gmail.com>
2024-03-11 21:02:44 +0800

    refactor drag n drop functionality

--- client/src/features/auth/Signup.jsx
+++ client/src/features/auth/Signup.jsx
@@ -5,6 +5,17 @@ import { Button } from '@/components/ui/button';
 import { useDropzone } from 'react-dropzone';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import { X } from 'lucide-react';
+
 const Signup = () => {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
@@ -13,22 +24,37 @@ const Signup = () => {
     previewImage: null,
     picture: null,
   });
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({
-    accept: {
-      'image/png': ['.png'],
-      'image/jpg': ['.jpg'],
-      'image/jpeg': ['.jpeg'],
-    },
-    onDrop: (acceptedFiles) => {
-      if (acceptedFiles.length === 0) {
-        console.error('No files were accepted or an error occurred.');
-        return;
-      }
-      const file = acceptedFiles[0];
-      const preview = URL.createObjectURL(file);
-      setFile({ picture: file, previewImage: preview });
-    },
-  });
+  const [error, setError] = useState('');
+  const { getRootProps, getInputProps, isDragActive, isDragReject } =
+    useDropzone({
+      accept: {
+        'image/jpeg': ['.jpeg', '.jpg', '.png'],
+      },
+      maxFiles: 1,
+      onDropRejected: (acceptedFiles) => {
+        if (acceptedFiles.length > 1) {
+          setError('Please only input one picture for your profile');
+        }
+        setFile({
+          previewImage: null,
+          picture: null,
+        });
+      },
+      onDropAccepted: (acceptedFiles) => {
+        const file = acceptedFiles[0];
+        const preview = URL.createObjectURL(file);
+        setError('');
+        setFile({ picture: file, previewImage: preview });
+      },
+    });
+
+  const handleRemoveInputPicture = () => {
+    setError('');
+    setFile({
+      previewImage: null,
+      picture: null,
+    });
+  };
 
   const handleSubmit = () => {
     console.log(previewImage[0]);
@@ -63,31 +89,58 @@ const Signup = () => {
           />
           <div className="grid w-full items-center gap-1.5">
             <Label htmlFor="picture" className="text-lg font-semibold mb-2">
-              Profile picture (Optional)
+              Profile picture{' '}
+              <span className="text-sm text-gray-500">(Optional)</span>
             </Label>
+
+            {/* i can create custom component out of this */}
             <div
-              className="border-2 border-dashed border-gray-300 rounded-md p-6 flex flex-col items-center justify-center cursor-pointer"
-              {...getRootProps()}
+              {...getRootProps({
+                className: `border-2 border-dashed ${
+                  isDragReject ? 'border-red-300' : 'border-gray-300'
+                } rounded-md p-6 flex flex-col items-center justify-center cursor-pointer`,
+              })}
             >
               <input {...getInputProps()} />
               {isDragActive ? (
-                <p className="text-gray-600">Drop the files here ...</p>
+                <>
+                  {isDragReject ? (
+                    <p className="text-red-500">Select</p>
+                  ) : (
+                    <p className="text-gray-400">Drop the files here...</p>
+                  )}
+                </>
               ) : (
-                <p className="text-gray-600">
-                  Drag and drop an image here, or click to select a file
+                <p className="text-gray-400">
+                  Drag n Drop an image here, or click to select a file
                 </p>
               )}
             </div>
             {file?.previewImage && (
-              <div className="mt-4">
-                <img
-                  src={file?.previewImage}
-                  alt="Preview"
-                  className="max-w-full h-auto"
-                />
+              <div className="p-4">
+                <div className="flex justify-between text-gray-500 text-sm p-2 w-11/12">
+                  <p>File</p>
+                  <p>Preview</p>
+                </div>
+                <div className="flex justify-between items-center">
+                  <div className="flex items-center justify-between w-11/12 bg-gray-50 p-2 ">
+                    <p>
+                      {file.picture.name} - {`${file.picture.size} bytes`}
+                    </p>
+                    <Avatar>
+                      <AvatarImage src={file.previewImage} alt="@shadcn" />
+                      <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+                  </div>
+                  <X
+                    className="h-4 w-4 text-gray-500 hover:cursor-pointer hover:text-red-500"
+                    onClick={handleRemoveInputPicture}
+                  />
+                </div>
               </div>
             )}
           </div>
+          {error && <p className="text-sm text-red-500">{error}</p>}
           <Button
             onClick={handleSubmit}
             className="mt-6 bg-blue-500 text-white hover:bg-blue-600 px-4 py-2 rounded-md transition duration-300"
